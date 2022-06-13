94fe358574675dadb46b0b668f25081f95d1d6e4
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-13 21:32:44 +0500

    refactor(Header): remove eslint rules in component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable react/jsx-no-comment-textnodes */
 import './Header.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
