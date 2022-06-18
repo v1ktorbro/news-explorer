a56b8b85a0fe3a4129953c8c92e5010e81c62b6e
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-18 19:09:28 +0500

    refactor(Footer): replace icon facebook to telegram

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,12 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 const githubIcon = require('../../images/github_icon.svg');
 const facebookIcon = require('../../images/facebook_icon.svg');
+const telegramIcon = require('../../images/telegram_icon.svg');
 
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__signature">&copy; 2020 Supersite, Powered by News API</p>
+      <p className="footer__signature">&copy; 2022 Supersite, Powered by News API</p>
       <nav className="footer__links footer__links_string">
         <ul className="footer__list footer__list_string">
           <li className="footer__list-item">
@@ -27,8 +28,8 @@ function Footer() {
             </a>
           </li>
           <li className="footer__list-item">
-            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="footer__link">
-              <img src={facebookIcon} className="footer__social-icon footer__social-icon_facebook" alt="лого фейсбука" />
+            <a href="https://t.me/viktorAbr" target="_blank" rel="noopener noreferrer" className="footer__link">
+              <img src={telegramIcon} className="footer__social-icon footer__social-icon_facebook" alt="лого телеграмм" />
             </a>
           </li>
         </ul>
