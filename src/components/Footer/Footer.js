3ac82bf6c8e34c2a98b5e574836196702f82cce6
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-30 08:24:16 +0500

    refactor(footer): semantic code style change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,11 @@ import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 
 const githubIcon = require('../../images/github_icon.svg');
-const facebookIcon = require('../../images/facebook_icon.svg');
 const telegramIcon = require('../../images/telegram_icon.svg');
+
+const URL_GITHUB = 'https://github.com/v1ktorbro/news-explorer';
+const URL_TELEGRAM = 'https://t.me/viktorAbr';
+const URL_YANDEX = 'https://practicum.yandex.ru/web';
 
 function Footer() {
   return (
@@ -16,19 +19,19 @@ function Footer() {
             <Link to="/#main" className="footer__link">Главная</Link>
           </li>
           <li className="footer__list-item">
-            <a href="https://praktikum.yandex.ru/" target="_blank" rel="noopener noreferrer" className="footer__link">Яндекс.Практикум</a>
+            <a href={URL_YANDEX} target="_blank" rel="noopener noreferrer" className="footer__link">Яндекс.Практикум</a>
           </li>
         </ul>
       </nav>
       <nav className="footer__links footer__links_social-icons">
         <ul className="footer__list footer__list_social-icons">
           <li className="footer__list-item">
-            <a href="https://github.com/v1ktorbro" target="_blank" rel="noopener noreferrer" className="footer__link">
+            <a href={URL_GITHUB} target="_blank" rel="noopener noreferrer" className="footer__link">
               <img src={githubIcon} className="footer__social-icon footer__social-icon_github" alt="лого гитхаба" />
             </a>
           </li>
           <li className="footer__list-item">
-            <a href="https://t.me/viktorAbr" target="_blank" rel="noopener noreferrer" className="footer__link">
+            <a href={URL_TELEGRAM} target="_blank" rel="noopener noreferrer" className="footer__link">
               <img src={telegramIcon} className="footer__social-icon footer__social-icon_facebook" alt="лого телеграмм" />
             </a>
           </li>
